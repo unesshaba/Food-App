152853acbe07dff6bd31b6bd4fceca29e31e8104
Aithaba Youness <Youness.Aithaba@emsi-edu.ma>
2024-05-28 16:57:10 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 import fs from 'node:fs/promises';
 import bodyParser from 'body-parser';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// Définir le répertoire de base
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,7 +22,7 @@ app.use((req, res, next) => {
 
 app.get('/meals', async (req, res) => {
   try {
-    const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+    const meals = await fs.readFile(path.join(__dirname, 'data', 'available-meals.json'), 'utf8');
     res.json(JSON.parse(meals));
   } catch (error) {
     res.status(500).json({ message: 'Failed to load meals.' });
@@ -32,10 +38,10 @@ app.post('/orders', async (req, res) => {
       id: (Math.random() * 1000).toString(),
     };
 
-    const orders = await fs.readFile('./data/orders.json', 'utf8');
+    const orders = await fs.readFile(path.join(__dirname, 'data', 'orders.json'), 'utf8');
     const allOrders = JSON.parse(orders);
     allOrders.push(newOrder);
-    await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));
+    await fs.writeFile(path.join(__dirname, 'data', 'orders.json'), JSON.stringify(allOrders));
     res.status(201).json({ message: 'Order created!' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to process order.' });
