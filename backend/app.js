b89366a1332906815e3b48709faea1c4625db2c7
Aithaba Youness <Youness.Aithaba@emsi-edu.ma>
2024-05-30 09:32:13 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,24 @@
-import fs from 'node:fs/promises';
-import bodyParser from 'body-parser';
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-
-// Définir le répertoire de base
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
+app.use('/api', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.get('/meals', async (req, res) => {
+app.use('/api/meals', async (req, res) => {
   try {
     const meals = await fs.readFile(path.join(__dirname, 'data', 'available-meals.json'), 'utf8');
     res.json(JSON.parse(meals));
@@ -29,7 +27,7 @@ app.get('/meals', async (req, res) => {
   }
 });
 
-app.post('/orders', async (req, res) => {
+app.use('/api/orders', async (req, res) => {
   try {
     const orderData = req.body.order;
 
@@ -48,13 +46,13 @@ app.post('/orders', async (req, res) => {
   }
 });
 
-app.use((req, res) => {
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  res.status(404).json({ message: 'Not found' });
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
